50d9f9064a124565acbd9e9857c8963a82a90bca
TomCarlson-NTAC <61517893+TomCarlson-NTAC@users.noreply.github.com>
2020-05-26 15:23:50 -0400

    Added CTRL support
    Added matrix for CTRL, but memory is getting low, so no matrix for the modifier. And CTRL key combo sends a CTRL.

--- Psion-Keyboard-Firmware/PsionKeymapUSB.h
+++ Psion-Keyboard-Firmware/PsionKeymapUSB.h
@@ -148,3 +148,26 @@ static int keyScancodeFNMods [NROWS] [NCOLS] = {
   { 0          ,0          ,0          ,0          ,0             ,0              ,0          ,0          ,0        ,0      ,0        ,0       }
 };
 
+static int keyScancodeCtrl [NROWS] [NCOLS] = {
+  { 0       ,KEY_SPACE ,KEY_UP ,KEY_COMMA ,KEY_LEFT      ,KEY_RIGHT      ,KEY_LSHIFT ,0          ,0        ,0      ,0        ,0       },
+  { KEY_1   ,KEY_2     ,KEY_3  ,KEY_4     ,KEY_5         ,KEY_6          ,0          ,0          ,0        ,0      ,0        ,0       },
+  { KEY_7   ,KEY_8     ,KEY_9  ,KEY_0     ,KEY_BACKSPACE ,KEY_SEMICOLON  ,0          ,0          ,0        ,0      ,0        ,0       },
+  { KEY_Q   ,KEY_W     ,KEY_E  ,KEY_R     ,KEY_T         ,KEY_Y          ,0          ,0          ,0        ,0      ,0        ,KEY_ESC },
+  { KEY_U   ,KEY_I     ,KEY_O  ,KEY_P     ,KEY_L         ,KEY_ENTER      ,0          ,0          ,0        ,0      ,KEY_MENU ,0       },
+  { KEY_TAB ,KEY_A     ,KEY_S  ,KEY_D     ,KEY_F         ,KEY_G          ,0          ,0          ,KEY_CTRL ,0      ,0        ,0       },
+  { KEY_H   ,KEY_J     ,KEY_K  ,KEY_M     ,KEY_PERIOD    ,KEY_DOWN       ,0          ,0          ,0        ,KEY_FN ,0        ,0       },
+  { KEY_Z   ,KEY_X     ,KEY_C  ,KEY_V     ,KEY_B         ,KEY_N          ,0          ,KEY_RSHIFT ,0        ,0      ,0        ,0       }
+};
+
+/*
+static int keyScancodeCtrlMods [NROWS] [NCOLS] = {
+  { 0        ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL   ,0          ,0          ,0        ,0        ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,0          ,0          ,0        ,0        ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,0          ,0          ,0        ,0        ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,0          ,0          ,0        ,0        ,KEY_CTRL   },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,0          ,0          ,0        ,KEY_CTRL ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,0          ,KEY_CTRL  ,0         ,0        ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0        ,KEY_CTRL ,0          ,0          ,0          ,KEY_CTRL ,0        ,0          },
+  { KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,KEY_CTRL ,0          ,KEY_CTRL   ,0          ,0        ,0        ,0          }
+};
+*/
